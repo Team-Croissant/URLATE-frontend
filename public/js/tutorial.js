78ea0ab5b02dbaa096438fe6ed05d612274a9cc4
HyeokjinKang <coupy@croissant.tech>
2021-09-27 01:27:43 +0900

    이제 pixelRatio와 상관없이 폰트가 항상 일정하게 표시됩니다.

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -382,7 +382,7 @@ const drawParticle = (n, x, y, j, d) => {
         let newY = cy - Math.round(p / 10);
         ctx.fillStyle = getJudgeStyle(j.toLowerCase(), p, cx, newY);
         ctx.strokeStyle = `rgba(255, 255, 255, ${1 - p / 100})`;
-        ctx.font = `600 ${window.innerHeight / 25}px Metropolis`;
+        ctx.font = `600 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.lineWidth = 2;
@@ -404,7 +404,7 @@ const drawParticle = (n, x, y, j, d) => {
       let newY = cy - Math.round(p / 10);
       ctx.fillStyle = getJudgeStyle("miss", p);
       ctx.strokeStyle = `rgba(255, 255, 255, ${1 - p / 100})`;
-      ctx.font = `600 ${window.innerHeight / 25}px Metropolis`;
+      ctx.font = `600 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
       ctx.lineWidth = 2;
@@ -542,7 +542,7 @@ const drawBullet = (n, x, y, a) => {
       ctx.fill();
       break;
     default:
-      ctx.font = `500 ${window.innerHeight / 25}px Metropolis`;
+      ctx.font = `500 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
       ctx.fillStyle = "#F55";
       ctx.textAlign = "left";
       ctx.textBaseline = "top";
@@ -610,7 +610,7 @@ const cntRender = () => {
       } else if (comboAlertMs + 600 <= Date.now() && comboAlertMs + 900 > Date.now()) {
         comboOpacity = (comboAlertMs + 900 - Date.now()) / 900;
       }
-      fontSize = (window.innerHeight / 100) * (30 - (comboAlertMs + 900 - Date.now()) / 90);
+      fontSize = (window.innerHeight / 100) * (30 - (comboAlertMs + 900 - Date.now()) / 90) * pixelRatio;
       ctx.beginPath();
       ctx.font = `700 ${fontSize}px Metropolis`;
       ctx.fillStyle = `rgba(200,200,200,${comboOpacity})`;
@@ -649,7 +649,7 @@ const cntRender = () => {
           ctx.fillStyle = "#111";
           ctx.font = `${renderTriggers[i].weight} ${renderTriggers[i].size} Metropolis, Pretendard Variable`;
           if (renderTriggers[i].size.indexOf("vh") != -1)
-            ctx.font = `${renderTriggers[i].weight} ${(window.innerHeight / 100) * Number(renderTriggers[i].size.split("vh")[0])}px Metropolis, Pretendard Variable`;
+            ctx.font = `${renderTriggers[i].weight} ${(window.innerHeight / 100) * Number(renderTriggers[i].size.split("vh")[0]) * pixelRatio}px Metropolis, Pretendard Variable`;
           ctx.textAlign = renderTriggers[i].align;
           ctx.textBaseline = renderTriggers[i].valign;
           ctx.fillText(renderTriggers[i].text, (canvas.width / 200) * (renderTriggers[i].x + 100), (canvas.height / 200) * (renderTriggers[i].y + 100));
@@ -719,7 +719,7 @@ const cntRender = () => {
     }
   } catch (e) {
     if (e) {
-      ctx.font = `500 ${window.innerHeight / 25}px Metropolis`;
+      ctx.font = `500 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
       ctx.fillStyle = "#F55";
       ctx.textAlign = "left";
       ctx.textBaseline = "top";
@@ -733,12 +733,12 @@ const cntRender = () => {
   } else {
     displayScore = score;
   }
-  ctx.font = `700 ${window.innerHeight / 25}px Metropolis`;
+  ctx.font = `700 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
   ctx.fillStyle = "#333";
   ctx.textAlign = "center";
   ctx.textBaseline = "top";
   ctx.fillText(numberWithCommas(`${Math.round(displayScore)}`.padStart(9, 0)), canvas.width / 2, canvas.height / 80);
-  ctx.font = `${window.innerHeight / 40}px Metropolis`;
+  ctx.font = `${window.innerHeight / 40 * pixelRatio}px Metropolis`;
   ctx.fillStyle = "#555";
   ctx.fillText(`${combo}x`, canvas.width / 2, canvas.height / 70 + canvas.height / 25);
   drawCursor();
@@ -833,7 +833,7 @@ const calculateResult = () => {
   }
   if (missPoint.length == 0) {
     missCtx.fillStyle = "#FFF";
-    ctx.font = `500 ${window.innerHeight / 30}px Metropolis`;
+    ctx.font = `500 ${window.innerHeight / 30 * pixelRatio}px Metropolis`;
     missCtx.textAlign = "right";
     missCtx.textBaseline = "bottom";
     missCtx.fillText("Perfect!", missCanvas.width - 10, missCanvas.height * 0.8 - 10);
@@ -865,7 +865,7 @@ const trackMouseSelection = (i, v1, v2, x, y) => {
         }
         break;
       default:
-        ctx.font = `500 ${window.innerHeight / 25}px Metropolis`;
+        ctx.font = `500 ${window.innerHeight / 25 * pixelRatio}px Metropolis`;
         ctx.fillStyle = "#F55";
         ctx.textAlign = "left";
         ctx.textBaseline = "top";
