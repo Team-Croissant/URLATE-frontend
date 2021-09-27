1c7de6e61ba427268b24c2e5583d9bb593e3f811
HyeokjinKang <coupy@croissant.tech>
2021-09-27 09:54:06 +0900

    결과화면의 버튼 리뉴얼

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1139,26 +1139,22 @@ const globalScrollEvent = (e) => {
 };
 
 const finish = () => {
-  if (confirm(returnToGame)) {
-    if (confirm(returnToGame)) {
-      fetch(`${api}/tutorial`, {
-        method: "PUT",
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.result != "success") {
-            alert(`Error occured.\n${data.error}`);
-          } else {
-            window.location.href = `${url}/game?initialize=0`;
-          }
-        })
-        .catch((error) => {
-          alert(`Error occured.\n${error}`);
-          console.error(`Error occured.\n${error}`);
-        });
-    }
-  }
+  fetch(`${api}/tutorial`, {
+    method: "PUT",
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.result != "success") {
+        alert(`Error occured.\n${data.error}`);
+      } else {
+        window.location.href = `${url}/game?initialize=0`;
+      }
+    })
+    .catch((error) => {
+      alert(`Error occured.\n${error}`);
+      console.error(`Error occured.\n${error}`);
+    });
 };
 
 document.onkeydown = (e) => {
